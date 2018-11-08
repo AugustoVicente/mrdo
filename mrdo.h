7e522b27cf7b3d0c0fa1cab4473ec5575f33ae1a
fabiocassidori <40472425+fabiocassidori@users.noreply.github.com>
2018-11-08 11:10:07 -0200

    Add files via upload

---
+++ mrdo.h
@@ -0,0 +1,31 @@
+
+
+#ifndef MRDO_H
+#define MRDO_H
+
+void gotoxy (int x, int y);
+
+void ShowConsoleCursor(int showFlag);
+
+void SetColor(int ForgC);
+
+char menuInicial();
+
+int contaFrutas(char url[]);
+
+void carregarFase(char url[], int *xFrutas, int *yFrutas);
+
+typedef struct{
+	
+	int x, y;
+	int ultimaDirecao;
+	char status;
+	
+}mrDo;
+
+char movimentoMrDo (mrDo Do, int contFrutas, int *xFrutas, int *yFrutas);
+
+free(xFrutas);
+free(yFrutas);
+
+#endif
